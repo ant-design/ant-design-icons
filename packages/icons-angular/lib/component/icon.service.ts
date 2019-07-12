1f0e3980f73ea6c7b11515a939ef61de49928f88
Wendell <wendzhue@gmail.com>
2019-07-12 15:34:53 +0800

    :bookmark: angular: fix dependency and release 8.0.1 (#68)

--- packages/icons-angular/lib/component/icon.service.ts
+++ packages/icons-angular/lib/component/icon.service.ts
@@ -1,7 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { HttpBackend, HttpClient } from '@angular/common/http';
-import { Inject, Optional, Renderer2, RendererFactory2, SecurityContext } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Inject, Optional, Renderer2, RendererFactory2 } from '@angular/core';
 import { of as observableOf, Observable } from 'rxjs';
 import { catchError, finalize, map, share, tap } from 'rxjs/operators';
 import { CachedIconDefinition, IconDefinition, ThemeType, TwoToneColorPalette, TwoToneColorPaletteSetter } from '../types';
@@ -60,8 +59,7 @@ export class IconService {
     protected _rendererFactory: RendererFactory2,
     @Optional() protected _handler: HttpBackend,
     // tslint:disable-next-line:no-any
-    @Optional() @Inject(DOCUMENT) protected _document: any,
-    protected sanitizer: DomSanitizer
+    @Optional() @Inject(DOCUMENT) protected _document: any
   ) {
     this._renderer = this._rendererFactory.createRenderer(null, null);
     if (this._handler) {
@@ -93,7 +91,7 @@ export class IconService {
    * @param literal
    */
   addIconLiteral(type: string, literal: string): void {
-    const [ name, namespace ] = getNameAndNamespace(type);
+    const [ _name, namespace ] = getNameAndNamespace(type);
     if (!namespace) {
       throw NameSpaceIsNotSpecifyError();
     }
@@ -154,12 +152,8 @@ export class IconService {
         ? `${this._assetsUrlRoot}assets/${namespace}/${name}.svg`
         : `${this._assetsUrlRoot}assets/${icon.theme}/${icon.name}.svg`;
 
-      const safeUrl = this.sanitizer.sanitize(SecurityContext.URL, url);
-
-      if (!safeUrl) { throw UrlNotSafeError(url); }
-
       // Wrap a `IconDefinition`, cache it, delete the shared work.
-      inProgress = this._http.get(safeUrl, { responseType: 'text' }).pipe(
+      inProgress = this._http.get(url, { responseType: 'text' }).pipe(
         map(literal => ({ ...icon, icon: literal })),
         tap(definition => this.addIcon(definition)),
         finalize(() => this._inProgressFetches.delete(type)),
