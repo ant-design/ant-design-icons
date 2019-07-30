d34e9fa35874d860ea7b2b479433aee2648a76f6
vagusX <vagusxl@gmail.com>
2019-07-30 16:51:30 +0800

    fix: insert styles for @ant-design/icons

--- packages/icons-react/src/index.ts
+++ packages/icons-react/src/index.ts
@@ -1,3 +1,13 @@
+import { insertCss } from 'insert-css';
+import { iconStyles } from './utils';
+
+let cssInjectedFlag = false;
+
+if (!cssInjectedFlag) {
+  insertCss(iconStyles);
+  cssInjectedFlag = true;
+}
+
 export * from './icons';
 export * from './components/twoTonePrimaryColor';
 export { default as createFromIconfontCN } from './components/IconFont';
--- packages/icons-react/src/utils.ts
+++ packages/icons-react/src/utils.ts
@@ -78,3 +78,42 @@ export const svgBaseProps = {
 export const svgBaseStyle = {
   verticalAlign: '-0.125em',
 };
+
+export const iconStyles = `
+.anticon {
+  display: inline-block;
+  color: inherit;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.anticon svg {
+  display: inline-block;
+}
+
+.anticon-spin {
+  display: inline-block;
+  -webkit-animation: loadingCircle 1s infinite linear;
+  animation: loadingCircle 1s infinite linear;
+}
+
+@-webkit-keyframes loadingCircle {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg)
+  }
+}
+
+@keyframes loadingCircle {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg)
+  }
+}
+`;
