02f77f44cda497052589bc2adc65daa085eb4df8
HeskeyBaozi <hezhiyu233@foxmail.com>
2019-08-08 23:42:43 +0800

    add prepublish

--- packages/icons-svg/tasks/clean.ts
+++ packages/icons-svg/tasks/clean.ts
@@ -1,5 +1,12 @@
 import del from 'del';
 
 export default function clean() {
-  return del(['src', 'inline-svg', 'docs/inline-svg', 'docs/list.md']);
+  return del([
+    'src',
+    'inline-svg',
+    'docs/inline-svg',
+    'docs/list.md',
+    'es',
+    'lib'
+  ]);
 }
