7d283c88948e8a7c582a9f0f01323f6032869480
vagusX <vagusxl@gmail.com>
2019-08-09 16:13:27 +0800

    fix: types for auto import

--- packages/icons-react/scripts/prepare.sh
+++ packages/icons-react/scripts/prepare.sh
@@ -2,5 +2,4 @@
 ls | grep -v .fatherrc.js | grep -v jest.config.js | grep -v .eslintrc.js | grep -e '.d.ts' -e '.js$' | xargs rm
 cp ./lib/icons/*.d.ts ./
 # keep one d.ts to make vscode auto import working with corrcet path
-rm ./lib/icons/*.d.ts
 rm ./es/icons/*.d.ts
