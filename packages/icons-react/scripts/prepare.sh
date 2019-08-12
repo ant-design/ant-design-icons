dc2eb57d70c28d0146b8cfb39fc668a3dda5c3a1
vagusX <vagusX@users.noreply.github.com>
2019-08-12 14:37:33 +0800

    fix: clean up d.ts (#95)

--- packages/icons-react/scripts/prepare.sh
+++ packages/icons-react/scripts/prepare.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ls | grep -v .fatherrc.js | grep -v jest.config.js | grep -v .eslintrc.js | grep -e '.d.ts' -e '.js$' | xargs rm
-cp ./lib/icons/*.d.ts ./
 # keep one d.ts to make vscode auto import working with corrcet path
 rm ./es/icons/*.d.ts
