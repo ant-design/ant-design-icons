4e9fc6e195d8c71863b19c2e2334640dfe0b2fef
HeskeyBaozi <hezhiyu233@foxmail.com>
2020-01-16 16:26:53 +0800

    svg: clean

--- packages/icons-svg/gulpfile.ts
+++ packages/icons-svg/gulpfile.ts
@@ -1,13 +1,13 @@
 import { series, parallel } from 'gulp';
-import cleanDirectories from './tasks/preProcess/cleanDirectories';
 import copyTemplates from './tasks/generate/copyTemplates';
 import generateLibraryEntry from './tasks/generate/generateLibraryEntry';
 import generateFilledIcons from './tasks/generate/generateFilledIcons';
 import generateOutlinedIcons from './tasks/generate/generateOutlinedIcons';
 import generateTwoToneIcons from './tasks/generate/generateTwoToneIcons';
+import { clean } from './tasks/preProcess/clean';
 
 export default series(
-  cleanDirectories,
+  clean(['src', 'inline-svg', 'es', 'lib']),
   parallel(
     copyTemplates,
     generateFilledIcons,
