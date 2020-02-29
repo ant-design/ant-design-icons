fe65de86e23d74532f08bb6b1677a9efcad9e763
zombiej <smith3816@gmail.com>
2020-02-29 21:20:53 +0800

    chore: Add umd support

--- packages/icons-react/.fatherrc.js
+++ packages/icons-react/.fatherrc.js
@@ -1,8 +1,13 @@
 export default {
   cjs: 'babel',
   esm: { type: 'babel', importLibToEs: true },
+  umd: {
+    globals: { react: 'window.React' },
+    minFile: false,
+    sourcemap: false,
+  },
   preCommit: {
     eslint: true,
     prettier: true,
   },
-}
+};
