80e05a5ff08577072c5d9dc51314db66e507c1a9
Baic <303394539@qq.com>
2020-04-22 09:29:38 +0800

    增加scriptUrl数组形式的单元测试

--- packages/icons-react/examples/use-iconfontcn.tsx
+++ packages/icons-react/examples/use-iconfontcn.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { createFromIconfontCN } from '../src';
 
 const IconFont = createFromIconfontCN({
-  // scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
-  scriptUrl: ['//at.alicdn.com/t/font_1728622_jaz3y5gulz.js', '//at.alicdn.com/t/font_1517974_d9tp7huqb5r.js'],
+  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+  // scriptUrl: ['//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js', '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js'],
 });
 
 const IconfontCN = () => (
   <div>
     <h1>Icons from iconfont.cn</h1>
-    <IconFont type="scanpay" />
+    <IconFont type="icon-tuichu" />
     <IconFont type="icon-facebook" />
     <IconFont type="icon-twitter" />
   </div>
