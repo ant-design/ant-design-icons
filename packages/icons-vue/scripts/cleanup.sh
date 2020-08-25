10330d624f62fdb8981de7a2fc499485b65be8c3
tanjinzhou <415800467@qq.com>
2020-08-25 11:00:13 +0800

    fix: add lib types

--- packages/icons-vue/scripts/cleanup.sh
+++ packages/icons-vue/scripts/cleanup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ls | grep -v .jest.js | grep -v .eslintrc.js | grep -e '.d.ts' -e '.js$' | xargs rm
 # keep one d.ts to make vscode auto import working with corrcet path
-rm ./lib/icons/*.d.ts || echo
 rm ./es/icons/*.d.ts || echo
