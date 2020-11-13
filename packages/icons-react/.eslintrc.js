e6b2e5dd5d74cacc76268ff0c726546c48f98c23
afc163 <afc163@gmail.com>
2020-11-13 11:41:14 +0800

    fix packages/icons-react/.eslintrc.js

--- packages/icons-react/.eslintrc.js
+++ packages/icons-react/.eslintrc.js
@@ -1,13 +1,9 @@
-const base = require('@umijs/fabric/dist/eslint');
-
 module.exports = {
-  ...base,
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   env: {
-    ...base.env,
     jest: true,
   },
   rules: {
-    ...base.rules,
     '@typescript-eslint/no-explicit-any': 0,
     'react/no-did-update-set-state': 0,
     'react/no-find-dom-node': 0,
