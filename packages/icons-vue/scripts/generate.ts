da3f19f64b2bc0a892bfdf033e3f59183a403c60
zkwolf <chenhao5866@gmail.com>
2020-11-26 10:09:37 +0800

    chore: support vue 3.0.3

--- packages/icons-vue/scripts/generate.ts
+++ packages/icons-vue/scripts/generate.ts
@@ -35,11 +35,11 @@ async function generateIcons() {
 // GENERATE BY ./scripts/generate.ts
 // DON NOT EDIT IT MANUALLY
 
-import { SetupContext } from 'vue';
+import { FunctionalComponent } from 'vue';
 import <%= svgIdentifier %>Svg from '@ant-design/icons-svg/lib/asn/<%= svgIdentifier %>';
 import AntdIcon, { AntdIconProps } from '../components/AntdIcon';
 
-const <%= svgIdentifier %> = (props: AntdIconProps, context: SetupContext) => {
+const <%= svgIdentifier %>: FunctionalComponent<AntdIconProps> = (props, context) => {
   const p = { ...props, ...context.attrs };
   return <AntdIcon {...p} icon={<%= svgIdentifier %>Svg}></AntdIcon>;
 };
