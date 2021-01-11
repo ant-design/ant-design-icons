fad72661f5a126db0144ca14a507e7c5d390161e
Championrunner <27343592+Ishaan28malik@users.noreply.github.com>
2021-01-11 22:40:01 +0530

    Create SolutionFilled.tsx

---
+++ packages/icons-react/src/icons/SolutionFilled.tsx
@@ -0,0 +1,14 @@
+// GENERATE BY ./scripts/generate.ts
+// DON NOT EDIT IT MANUALLY
+
+import React from 'react'
+import SolutionOutlinedSvg from '@ant-design/icons-svg/lib/asn/SolutionFilled';
+import AntdIcon, { AntdIconProps } from '../components/AntdIcon';
+
+const SolutionFilled = (
+  props: AntdIconProps,
+  ref: React.MutableRefObject<HTMLSpanElement>,
+) => <AntdIcon {...props} ref={ref} icon={SolutionFilledSvg} />;
+
+SolutionFilled.displayName = 'SolutionFilled';
+export default React.forwardRef<HTMLSpanElement, AntdIconProps>(SolutionFilled);
