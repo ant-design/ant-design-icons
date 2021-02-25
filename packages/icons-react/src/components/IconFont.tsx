f60dd9b167a3571d31987aa89d32918b132f9d64
limingxin <906529775@qq.com>
2021-02-25 15:03:01 +0800

    add iconFontTypes to get all iconfont types
    use fetch to load scriptUrl and match all iconfont types

--- packages/icons-react/src/components/IconFont.tsx
+++ packages/icons-react/src/components/IconFont.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Icon, { IconBaseProps } from './Icon';
 
 const customCache = new Set<string>();
+const iconFontTypes = new Set<string>();
 
 export interface CustomIconOptions {
   scriptUrl?: string | string[];
@@ -12,12 +13,36 @@ export interface IconFontProps extends IconBaseProps {
   type: string;
 }
 
+interface CompoundedComponent
+  extends React.ForwardRefExoticComponent<
+    IconFontProps & React.RefAttributes<HTMLSpanElement>
+  > {
+  iconFontTypes: Set<string>;
+}
+
 function isValidCustomScriptUrl(scriptUrl: string): boolean {
   return Boolean(
     typeof scriptUrl === 'string'
       && scriptUrl.length
       && !customCache.has(scriptUrl)
   );
+}
+
+function getIconFontTypes(scriptUrl: string): void {
+  try {
+    // will load scriptUrl form disk cache
+    fetch(scriptUrl)
+      .then((r) => r.text())
+      .then((result) => {
+          // Since Safari doesn't support trailing assertion, matchall is used here. See here for details：https://stackoverflow.com/a/51568859
+          const matches = result.matchAll(/(?:<symbol id=")(\S*)(?=")/g);
+          for (const match of matches) {
+            IconList.add(match[1])
+          }
+      });
+  } catch (e) {
+    console.log("iconfont types load failed", e);
+  }
 }
 
 function createScriptUrlElements(scriptUrls: string[], index: number = 0): void {
@@ -28,6 +53,7 @@ function createScriptUrlElements(scriptUrls: string[], index: number = 0): void 
     script.setAttribute('data-namespace', currentScriptUrl);
     if (scriptUrls.length > index + 1) {
       script.onload = () => {
+        getIconFontTypes(currentScriptUrl);
         createScriptUrlElements(scriptUrls, index + 1);
       };
       script.onerror = () => {
@@ -78,9 +104,10 @@ export default function create(options: CustomIconOptions = {}): React.SFC<IconF
         {content}
       </Icon>
     );
-  });
+  }) as CompoundedComponent;
 
   Iconfont.displayName = 'Iconfont';
+  Iconfont.iconFontTypes = iconFontTypes;
 
   return Iconfont;
 }
