4666e7f0c881c3732af5196d0e0f6f881e8c9e17
zombiej <smith3816@gmail.com>
2021-09-24 18:13:08 +0800

    feat: Add holder icon

---
+++ packages/icons-react/src/icons/HolderOutlined.tsx
@@ -0,0 +1,14 @@
+// GENERATE BY ./scripts/generate.ts
+// DON NOT EDIT IT MANUALLY
+
+import * as React from 'react'
+import HolderOutlinedSvg from '@ant-design/icons-svg/lib/asn/HolderOutlined';
+import AntdIcon, { AntdIconProps } from '../components/AntdIcon';
+
+const HolderOutlined = (
+  props: AntdIconProps,
+  ref: React.MutableRefObject<HTMLSpanElement>,
+) => <AntdIcon {...props} ref={ref} icon={HolderOutlinedSvg} />;
+
+HolderOutlined.displayName = 'HolderOutlined';
+export default React.forwardRef<HTMLSpanElement, AntdIconProps>(HolderOutlined);
