996040119c34c3fad203174267df3e007ca50789
Jørn Are Hatlelid <jornare@hatlelid.net>
2023-05-09 10:21:36 +0200

    Angular 16 support (#564)
    * Angular 16 support
    
    * formatting
    
    * Update package.json
    
    fixed wrong version of eslint-plugin

--- packages/icons-angular/src/component/icon.module.ts
+++ packages/icons-angular/src/component/icon.module.ts
@@ -1,10 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IconDirective } from './icon.directive';
 import { IconService } from './icon.service';
 
 @NgModule({
-  imports: [CommonModule],
   exports: [IconDirective],
   declarations: [IconDirective],
   providers: [IconService]
