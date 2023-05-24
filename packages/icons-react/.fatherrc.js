49a8c618c3bed3d85115f3eafe349e74ca984bf0
二货机器人 <smith3816@gmail.com>
2023-05-24 17:16:43 +0800

    chore: update react externals config & bump

--- packages/icons-react/.fatherrc.js
+++ packages/icons-react/.fatherrc.js
@@ -7,7 +7,7 @@ const config = {
 
 if (process.env.NODE_ENV !== 'ci') {
   config.umd = {
-    externals: { react: 'window.React' },
+    externals: { react: 'React' },
     sourcemap: false,
   };
 }
