02c35be16558b0158d8de678dd1d25de9090c96d
Peach <scdzwyxst@gmail.com>
2023-05-30 09:38:51 +0800

    build: keep umd dist unchanged (#577)

--- packages/icons-react/.fatherrc.js
+++ packages/icons-react/.fatherrc.js
@@ -7,6 +7,26 @@ const config = {
 
 if (process.env.NODE_ENV !== 'ci') {
   config.umd = {
+    entry: {
+      'index.umd': {
+        chainWebpack(memo) {
+          memo.entryPoints.clear();
+          memo.entry('index.umd').add('./src/index.ts');
+          memo.optimization.minimize(false);
+
+          return memo;
+        },
+      },
+      'index.umd.min': {
+        chainWebpack(memo) {
+          memo.entryPoints.clear();
+          memo.entry('index.umd.min').add('./src/index.ts');
+
+          return memo;
+        },
+      },
+    },
+    output: 'dist',
     name: 'icons',
     externals: { react: 'React' },
     sourcemap: false,
