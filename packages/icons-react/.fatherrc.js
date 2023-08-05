916efcc3cbac7044b6e5508d68e7fff2dae3faf9
MadCcc <1075746765@qq.com>
2023-08-05 14:16:37 +0800

    chore: use default config for es

--- packages/icons-react/.fatherrc.js
+++ packages/icons-react/.fatherrc.js
@@ -3,13 +3,6 @@ import { defineConfig } from 'father';
 const config = defineConfig({
   // Locked version only supports 1.0.0
   plugins: ['@rc-component/father-plugin'],
-  esm: {
-    transformer: 'swc',
-    platform: 'browser',
-    targets: {
-      ie: 11,
-    },
-  },
   cjs: {
     transformer: 'swc',
     targets: {
