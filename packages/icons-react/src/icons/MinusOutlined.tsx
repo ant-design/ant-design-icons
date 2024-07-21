e2b1dd62a66028c5b49f00ca0cb17b2eb9b41a95
红 <wxh1220@gmail.com>
2024-07-21 14:42:03 +0800

    chore(build): JSDoc svg to png base64 (#646)
    * chore(build): svg to png base64
    
    * chore(build): generate

--- packages/icons-react/src/icons/MinusOutlined.tsx
+++ packages/icons-react/src/icons/MinusOutlined.tsx
@@ -10,7 +10,7 @@ const MinusOutlined = (
   ref: React.MutableRefObject<HTMLSpanElement>,
 ) => <AntdIcon {...props} ref={ref} icon={MinusOutlinedSvg} />;
 
- /**![minus](data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNTAiIGhlaWdodD0iNTAiIGZpbGw9IiNjYWNhY2EiIHZpZXdCb3g9IjY0IDY0IDg5NiA4OTYiIGZvY3VzYWJsZT0iZmFsc2UiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTg3MiA0NzRIMTUyYy00LjQgMC04IDMuNi04IDh2NjBjMCA0LjQgMy42IDggOCA4aDcyMGM0LjQgMCA4LTMuNiA4LTh2LTYwYzAtNC40LTMuNi04LTgtOHoiIC8+PC9zdmc+) */ 
+ /**![minus](data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAABkElEQVR4Ae3gAZAkSZIkSRKLqpm7R0REZmZmVlVVVVV3d3d3d/fMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMzMdHd3d3dXV1VVVVVmZkZGRIS7m5kKz0xmV3d1d3dPz8zMzMxMovi/AfF/A+L/BsT/DYj/GxD/NyD+b0D834D4vwHxfwPi/wbE/w2I/xsQ/zcg/m9A/N+A+L8B8X8D4v8GxP8NiP8bEP83IP5vQDyXP/mTP/lsSZ/F/2C2P+eVXumVPptnQzyXP/mTP/lsSZ/F/2C2P+eVXumVPptnQzyXP/mTP/lsSZ/F/2C2P+eVXumVPptnQzyXP/mTP/lsSZ/F/2C2P+eVXumVPptnQ/zfgPi/AfF/A+L/BsT/DYj/GxD/NyD+b0D834D4vwHxfwPi/wbE/w2I/xsQ/zcg/m9A/N+A+L8B8X8D4v8GxP8NiP8bEP838I+ZHyAza0LrGAAAAABJRU5ErkJggg==) */ 
 const RefIcon: React.ForwardRefExoticComponent<
   Omit<AntdIconProps, 'ref'> & React.RefAttributes<HTMLSpanElement>
 > = React.forwardRef<HTMLSpanElement, AntdIconProps>(MinusOutlined);
