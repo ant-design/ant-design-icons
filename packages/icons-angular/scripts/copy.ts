a8b2dcd3370d48001a04bfcebaaa57533145db2f
Laffery <laffery86@gmail.com>
2025-05-29 17:16:53 +0800

    chore: update npm scripts

--- packages/icons-angular/scripts/copy.ts
+++ packages/icons-angular/scripts/copy.ts
@@ -2,9 +2,9 @@ const fs = require('fs-extra');
 const path = require('path');
 
 function copyAssets() {
-    const from = path.resolve(__dirname, '../src/inline-svg');
-    const to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
-    fs.copy(from, to);
+  const from = path.resolve(__dirname, '../src/inline-svg');
+  const to = path.resolve(__dirname, '../dist/icons/src/inline-svg');
+  fs.copy(from, to);
 }
 
 copyAssets();
