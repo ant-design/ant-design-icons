452db4359095c18d3186ac1a2025e086fe0c350d
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2026-04-22 15:01:45 +0800

    chore: replace happy-dom with jsdom in package.json and vitest configuration

--- packages/icons-react/vitest.config.ts
+++ packages/icons-react/vitest.config.ts
@@ -2,16 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'happy-dom',
-    environmentOptions: {
-      happyDOM: {
-        settings: {
-          // Enable iconfont.cn support, which requires loading and evaluating JavaScript.
-          disableJavaScriptFileLoading: false,
-          disableJavaScriptEvaluation: false,
-        }
-      }
-    },
+    environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setupTests.ts'],
     include: ['tests/**/*.{test,spec}.{ts,tsx,js,jsx}'],
