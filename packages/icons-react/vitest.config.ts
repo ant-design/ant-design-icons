1de4366973a9058f1b6015a1311dffe3776ee253
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2026-04-22 14:53:13 +0800

    chore: add environment options for happy-dom in Vitest configuration

--- packages/icons-react/vitest.config.ts
+++ packages/icons-react/vitest.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    environmentOptions: {
+      happyDOM: {
+        settings: {
+          // Enable iconfont.cn support, which requires loading and evaluating JavaScript.
+          disableJavaScriptFileLoading: false,
+          disableJavaScriptEvaluation: false,
+        }
+      }
+    },
     globals: true,
     setupFiles: ['./tests/setupTests.ts'],
     include: ['tests/**/*.{test,spec}.{ts,tsx,js,jsx}'],
