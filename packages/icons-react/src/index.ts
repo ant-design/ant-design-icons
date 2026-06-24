fd2f9c887f4a88bcc45994c1fdbe6e145fddf284
afc163 <afc163@gmail.com>
2026-06-24 12:25:42 +0800

    chore: mark react type exports

--- packages/icons-react/src/index.ts
+++ packages/icons-react/src/index.ts
@@ -1,7 +1,8 @@
 import Context from './components/Context';
 
 export * from './icons';
-export * from './components/twoTonePrimaryColor';
+export { getTwoToneColor, setTwoToneColor } from './components/twoTonePrimaryColor';
+export type { TwoToneColor } from './components/twoTonePrimaryColor';
 export { default as createFromIconfontCN } from './components/IconFont';
 export { default } from './components/Icon';
 export const IconProvider = Context.Provider;
